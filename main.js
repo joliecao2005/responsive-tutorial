a69ba6da07e5afb2469f55ca3147cb3e9bd7d1e5
joliecao865 <lhp080605@gmail.com>
2025-10-01 18:57:38 +1000

    class

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const modeButton = document.getElementById("modeButton");
 
+// this will keep track of dark vs light mode
+let isLightMode = true;
+
 // function to toggle between dark mode and light mode
 // takes in bool : if true set to dark if false set to light
-function switchModes(toDark){
-  if(toDark){
+function switchModes(toDark) {
+  if (toDark) {
     modeButton.textContent = "Light Mode";
   } else {
     modeButton.textContent = "Dark Mode";
   }
+}
+
+modeButton.addEventListener("click", () => {
+  switchModes(isLightMode);
+});
+
+// test to see if user prefers dark mode
+// if so update the isLightMode variable, and our button text
+let userModePreference = window.matchMedia("(prefers-color-scheme:dark)");
+console.log(userModePreference.matches);
+// if this statement is true, user says they want dark mode
+if (userModePreference.matches) {
+  isLightMode = false;
+  switchModes(isLightMode);
 }
